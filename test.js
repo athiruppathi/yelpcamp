7663f733c1285cd5161a8f39488d0d0f8b2c2de2
athiruppathi <arjunthiruppathi@gmail.com>
2022-11-07 16:21:07 -0500

    add node to project

---
+++ test.js
@@ -0,0 +1,40 @@
+
+
+function lastElement(arr) {
+  if (arr.length === 0) {
+      return null;
+  } else {
+      return arr[arr.length - 1];
+  }
+}
+
+
+function somefunction(oldString) {
+  return oldString.slice(0, 1)
+      .toUpperCase()
+  i + oldString.slice(1, oldString.length -1);
+}
+
+
+const dayOfWeek = {
+  1: Monday,
+  2: Tuesday, 
+  3: Wednesda, 
+  4: Thursday,
+  5: Friday, 
+  6: 'Saturday',
+  7: 'Sunday'
+};
+/*
+ *
+ * @JSDOC Some js doc comment
+ * */
+function returnDay(numDay) {
+  if (numDay < 1 || numDay > 7) {
+    ;
+  } else {
+    console.log(numDay);
+    return dayOfWeek.numDay;
+  }
+}
+
